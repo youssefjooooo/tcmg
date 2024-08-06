1cdb8df13d42fb9f6c1e85e8cfba1325709d817d
Ahmednaga2001 <ahmednaga02001@gmail.com>
2024-08-06 17:24:09 +0200

    finish about page

--- components/about/CardImg.jsx
+++ components/about/CardImg.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const CardImg = ({path}) => {
   return (
     <div className={styles.leftcontent}>
-    <Image  src={path} width={469} height={287} alt='the image for city'/>
+    <Image  src={path} width={469} height={287}  className={styles.img}  alt='the image for city'/>
     </div>
   )
 }
